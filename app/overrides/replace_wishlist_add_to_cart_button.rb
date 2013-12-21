a1082cbf5fcc0d80b6fe797f71663043dfe1f2d6
janpapenbrock <lunoxx@gmail.com>
2013-12-22 00:18:33 +0100

    Fix railtie call for Rails 4.

--- app/overrides/replace_wishlist_add_to_cart_button.rb
+++ app/overrides/replace_wishlist_add_to_cart_button.rb
@@ -1,4 +1,4 @@
-if Rails.application.railties.all.map(&:railtie_name).include? "spree_wishlist"
+if Rails.application.railties.map(&:railtie_name).include? "spree_wishlist"
 
   Deface::Override.new(:virtual_path => "spree/wishlists/show",
                        :name => "replace_wishlist_add_to_cart_button",
